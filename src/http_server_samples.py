df742b4e4f5198e032ec3ccb4711045c493fe6f4
Matt Jeanes <mattjeanes23@gmail.com>
2020-09-15 00:00:24 +0100

    Load many models

--- src/http_server_samples.py
+++ src/http_server_samples.py
@@ -1,49 +1,83 @@
 #!/usr/bin/env python3
 
+import encoder
+import sample
+import model
 import fire
 import json
 import os
+import sys
+import gc
 import numpy as np
 import tensorflow as tf
 import flask
 from flask import request, jsonify, make_response
+tf.logging.set_verbosity(tf.logging.ERROR)
 
-import model, sample, encoder
+current_model_name = None
+seed = None
+nsamples = 1
+batch_size = 1
+length = 50
+temperature = 1
+top_k = 0
+top_p = 0.0
+if batch_size is None:
+    batch_size = 1
+assert nsamples % batch_size == 0
 
-def interact_model(
-    model_name='117M',
-    seed=None,
-    nsamples=1,
-    batch_size=1,
-    length=None,
-    temperature=1,
-    top_k=0,
-    top_p=0.0
-):
-    """
-    Interactively run the model
-    :model_name=117M : String, which model to use
-    :seed=None : Integer seed for random number generators, fix seed to reproduce
-     results
-    :nsamples=1 : Number of samples to return total
-    :batch_size=1 : Number of batches (only affects speed/memory).  Must divide nsamples.
-    :length=None : Number of tokens in generated text, if None (default), is
-     determined by model hyperparameters
-    :temperature=1 : Float value controlling randomness in boltzmann
-     distribution. Lower temperature results in less random completions. As the
-     temperature approaches zero, the model will become deterministic and
-     repetitive. Higher temperature results in more random completions.
-    :top_k=0 : Integer value controlling diversity. 1 means only 1 word is
-     considered for each step (token), resulting in deterministic completions,
-     while 40 means 40 words are considered at each step. 0 (default) is a
-     special setting meaning no restrictions. 40 generally is a good value.
-    :top_p=0.0 : Float value controlling diversity. Implements nucleus sampling,
-     overriding top_k if set to a value > 0. A good setting is 0.9.
-    """
-    if batch_size is None:
-        batch_size = 1
-    assert nsamples % batch_size == 0
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+config.log_device_placement = True
+sessStart = None
+sess = None
+context = None
+output = None
+enc = None
 
+app = flask.Flask(__name__)
+# app.config["DEBUG"] = True
+
+
+@app.route('/api/generate', methods=['POST'])
+def home():
+    raw_text = request.json.get('text', '')
+    model_name = request.json.get('model', '')
+    if len(raw_text) == 0:
+        raw_text = '<|endoftext|>'
+    if model_name != current_model_name:
+        load_model(model_name)
+    context_tokens = enc.encode(raw_text)
+    generated = 0
+    for _ in range(nsamples // batch_size):
+        out = sess.run(output, feed_dict={
+            context: [context_tokens for _ in range(batch_size)]
+        })[:, len(context_tokens):]
+        for i in range(batch_size):
+            generated += 1
+            text = enc.decode(out[i])
+            print("Input: " + raw_text)
+            print("=" * 40 + " SAMPLE " + str(generated) + " " + "=" * 40)
+            print(text)
+            return make_response(
+                jsonify(
+                    {"text": text}
+                ),
+                200
+            )
+
+
+def load_model(model_name):
+    global  sess, context, output, length, enc, current_model_name
+    if not os.path.exists(os.path.join('models', model_name)):
+        raise Exception("Invalid model")
+    if sess is not None:
+        sess.close()
+        sess = None
+        context = None
+        output = None
+        enc = None
+        gc.collect()
     enc = encoder.get_encoder(model_name)
     hparams = model.default_hparams()
     with open(os.path.join('models', model_name, 'hparams.json')) as f:
@@ -52,54 +86,23 @@ def interact_model(
     if length is None:
         length = hparams.n_ctx // 2
     elif length > hparams.n_ctx:
-        raise ValueError("Can't get samples longer than window size: %s" % hparams.n_ctx)
+        raise ValueError(
+            "Can't get samples longer than window size: %s" % hparams.n_ctx)
+    sess = tf.Session(config=config)
+    context = tf.placeholder(tf.int32, [batch_size, None])
+    np.random.seed(seed)
+    tf.set_random_seed(seed)
+    output = sample.sample_sequence(
+        hparams=hparams, length=length,
+        context=context,
+        batch_size=batch_size,
+        temperature=temperature, top_k=top_k, top_p=top_p
+    )
 
-    config = tf.ConfigProto()
-    config.gpu_options.allow_growth = True
-    config.log_device_placement = True
-    with tf.Session(graph=tf.Graph(), config=config) as sess:
-        context = tf.placeholder(tf.int32, [batch_size, None])
-        np.random.seed(seed)
-        tf.set_random_seed(seed)
-        output = sample.sample_sequence(
-            hparams=hparams, length=length,
-            context=context,
-            batch_size=batch_size,
-            temperature=temperature, top_k=top_k, top_p=top_p
-        )
+    saver = tf.train.Saver()
+    ckpt = tf.train.latest_checkpoint(os.path.join('models', model_name))
+    saver.restore(sess, ckpt)
+    current_model_name = model_name
 
-        saver = tf.train.Saver()
-        ckpt = tf.train.latest_checkpoint(os.path.join('models', model_name))
-        saver.restore(sess, ckpt)
 
-        app = flask.Flask(__name__)
-        # app.config["DEBUG"] = True
-
-        @app.route('/api/generate', methods=['POST'])
-        def home():
-            raw_text = request.json.get('text', '')
-            context_tokens = enc.encode(raw_text)
-            generated = 0
-            for _ in range(nsamples // batch_size):
-                out = sess.run(output, feed_dict={
-                    context: [context_tokens for _ in range(batch_size)]
-                })[:, len(context_tokens):]
-                for i in range(batch_size):
-                    generated += 1
-                    text = enc.decode(out[i])
-                    print("Input: " + raw_text)
-                    print("=" * 40 + " SAMPLE " + str(generated) + " " + "=" * 40)
-                    print(text)
-                    return make_response(
-                        jsonify(
-                            {"text": text}
-                        ),
-                        200
-                    )
-
-        app.run(host='0.0.0.0')
-
-        
-
-if __name__ == '__main__':
-    fire.Fire(interact_model)
+app.run(host='0.0.0.0')
